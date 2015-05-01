d08cbdf348a2d52bfcaba10958f2a9f8758fedc4
Chris Cannon <ccannon@50onred.com>
2015-05-01 17:12:35 -0400

    Additional tests.

--- tests/highlander_tests.py
+++ tests/highlander_tests.py
@@ -1,9 +1,10 @@
 from os import unlink
 from tempfile import mkstemp
 from unittest import TestCase
-from highlander import InvalidPidFileError
+from os.path import isfile
+from highlander import InvalidPidFileError, PidFileExistsError
 
-from highlander.highlander import _read_pid_file
+from highlander.highlander import _read_pid_file, _delete, _set_running
 from highlander import one
 
 
@@ -27,7 +28,7 @@ class HighlanderTests(TestCase):
         try:
             with open(filename, 'w') as f:
                 f.write('abc,def')
-            self.assertRaises(InvalidPidFileError, _read_pid_file(filename))
+            self.assertRaises(InvalidPidFileError, _read_pid_file, filename)
         finally:
             unlink(filename)
 
@@ -36,3 +37,15 @@ class HighlanderTests(TestCase):
         def f():
             print 'hello'
         f()
+
+    def delete_valid_file_test(self):
+        _, f = mkstemp()
+        _delete(f)
+        self.assertFalse(isfile(f))
+
+    def delete_invalid_file_test(self):
+        self.assertRaises(InvalidPidFileError, _delete, 'not_a_file')
+
+    def running_file_exists_test(self):
+        _, f = mkstemp()
+        self.assertRaises(PidFileExistsError, _set_running, f)
