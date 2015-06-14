60aff130859f35c313b4fa0795bdfd37e4353b10
Chris Cannon <christophertcannon@gmail.com>
2015-06-14 13:17:15 -0400

    Use a directory name that is too long.

--- tests/highlander_tests.py
+++ tests/highlander_tests.py
@@ -146,7 +146,7 @@ class HighlanderTestCase(TestCase):
         self.assertEquals('/test/INFO', _get_pid_filename('/test'))
 
     def test_other_os_error(self):
-        self.assertRaises(OSError, _is_running, '/')
+        self.assertRaises(OSError, _is_running, 'a'*1000)
 
 
 def get_suite():
